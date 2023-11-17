b44b22a27aa08fe16e7b9c6ceb3e0c65380dcd2e
Pedro <pdihax@gmail.com>
2023-11-17 17:03:38 -0300

    Finish docker compose and is working

--- migration/setup.sh
+++ migration/setup.sh
@@ -1,2 +1,13 @@
+export PGPASSWORD=$PSQL_PASSWORD
+
+until psql -h $PSQL_HOST -p $PSQL_PORT -U postgres -d $PSQL_DB_NAME -c '\q'; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+psql -h $PSQL_HOST -p $PSQL_PORT -U postgres -d $PSQL_DB_NAME -a -f ./ITBA_2023_esquema_facturacion.sql
+psql -h $PSQL_HOST -p $PSQL_PORT -U postgres -d $PSQL_DB_NAME -a -f ./UPDATE_esquema.sql
+
+echo "Data uploaded"
+
 pip install -r ./requirements.txt
 python ./migration.py
