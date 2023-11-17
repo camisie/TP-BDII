d947639cc55c5ff7095992ca66f0bf8dd99fc155
Martín E. Zahnd <mzahnd@itba.edu.ar>
2023-11-17 18:58:42 -0300

    PostgreSQL connection envs

--- api/persistence/postgresql/PsycopgCursor.py
+++ api/persistence/postgresql/PsycopgCursor.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 import sys
+from os import environ
 
 import psycopg
 
 
 class PsycopgCursor:
     def __init__(self):
-        pass
+        self.psql_params = {
+            "dbname": environ.get("PSQL_DB_NAME", "e01"),
+            "user": environ.get("PSQL_USER", "postgres"),
+            "password": environ.get("PSQL_PASSWORD", "12345"),
+            "host": environ.get("PSQL_HOST", "127.0.0.1"),
+            "port": environ.get("PSQL_PORT", "5432"),
+        }
 
     async def __aenter__(self):
-        self.aconn = await psycopg.AsyncConnection.connect("dbname=bd2-tpo")
+        self.aconn = await psycopg.AsyncConnection.connect(**self.psql_params)
         self.cursor = self.aconn.cursor()
         return (self.aconn, self.cursor)
 
